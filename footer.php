8355e21cf3d7950e277513cfeecf8209345a7ca7
t-tamaru <t-tamaru@sakura.ad.jp>
2021-02-28 14:36:26 +0900

    footerのhtml実装

--- footer.php
+++ footer.php
@@ -10,15 +10,30 @@
  */
 
 ?>
-<?php do_action('bbfb_footer') ?>
 
-<!--
-<p id="page-top">
-    <a href="#wrap">
-        Page Top
-    </a>
-</p>
--->
+    <footer class="footer">
+        <div class="footer__banner">
+            <a href="<?php echo home_url('/contact/'); ?>">
+                <picture>
+                    <source srcset="<?php echo get_template_directory_uri(); ?>/dist/images/footer_banner.webp" type="image/webp">
+                    <img src="<?php echo get_template_directory_uri(); ?>/dist/images/footer_banner.jpg" srcset="<?php echo get_template_directory_uri(); ?>/dist/images/footer_banner.jpg 1x, <?php echo get_template_directory_uri(); ?>/dist/images/home/footer_banner@2x.jpg 2x " alt="">
+                </picture>
+            </a>
+        </div>
+        <picture>
+            <source srcset="<?php echo get_template_directory_uri(); ?>/dist/images/footer_logo.webp" type="image/webp">
+            <img src="<?php echo get_template_directory_uri(); ?>/dist/images/footer_logo.jpg" srcset="<?php echo get_template_directory_uri(); ?>/dist/images/footer_logo.jpg 1x, <?php echo get_template_directory_uri(); ?>/dist/images/home/footer_logo.jpg 2x " alt="">
+        </picture>
+        <div class="footer__menu">
+            <?php
+                wp_nav_menu(array(
+                    'theme_location' => 'primary'
+                ));
+            ?>
+        </div>
+        <small class="footer__copyright">© KAKAWARI All Rights Reserved</small>
+    </footer>
+</div><!-- #page -->
 
 <script src="<?php bloginfo('template_url'); ?>/js/jquery.matchHeight.js"></script>
 <script src="<?php bloginfo('template_url'); ?>/js/wow.js"></script>
@@ -28,3 +43,4 @@
 
 </body>
 </html>
+
